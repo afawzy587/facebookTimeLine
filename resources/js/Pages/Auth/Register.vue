11d3b8f9436697511528399c26e6163c7228fc5c
Ahmed fawzy Abdelaziz <46196049+afawzy587@users.noreply.github.com>
2024-03-26 15:57:01 +0200

    add national_id to register form

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -9,6 +9,7 @@ import { Head, Link, useForm } from '@inertiajs/vue3';
 const form = useForm({
     name: '',
     email: '',
+    national_id: '',
     password: '',
     password_confirmation: '',
 });
@@ -54,6 +55,22 @@ const submit = () => {
                 />
 
                 <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div>
+                <InputLabel for="national_id" value="National id" />
+
+                <TextInput
+                    id="national_id"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.national_id"
+                    required
+                    autofocus
+                    autocomplete="national_id"
+                />
+
+                <InputError class="mt-2" :message="form.errors.national_id" />
             </div>
 
             <div class="mt-4">
